8370210bf2535265695a6238fe10d08a9b4a5d20
LegoMaster3650 <37251737+LegoMaster3650@users.noreply.github.com>
2023-02-17 15:28:45 -0600

    1.0.3 - Tweaked logging in overlay rendering, fixed keybinding

--- src/main/java/io/_3650/itemupgrader/client/ItemUpgraderClient.java
+++ src/main/java/io/_3650/itemupgrader/client/ItemUpgraderClient.java
@@ -1,33 +1,24 @@
 package io._3650.itemupgrader.client;
 
-import org.slf4j.Logger;
-
-import com.mojang.logging.LogUtils;
-
 import io._3650.itemupgrader.client.renderer.ItemUpgraderClientReloader;
 import net.minecraft.client.Minecraft;
 import net.minecraft.server.packs.resources.ReloadableResourceManager;
+import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 public class ItemUpgraderClient {
 	
-	private static final Logger LOGGER = LogUtils.getLogger();
-	
 	public ItemUpgraderClient() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		bus.addListener(this::clientSetup);
+		bus.addListener(this::onKeyMappings);
 		if (Minecraft.getInstance().getResourceManager() instanceof ReloadableResourceManager manager) {
 			manager.registerReloadListener(ItemUpgraderClientReloader.INSTANCE);
 		}
 	}
 	
-	public void clientSetup(FMLClientSetupEvent event) {
-		event.enqueueWork(() -> {
-			LOGGER.debug("Item Upgrader client init");
-			//but nobody came...
-		});
+	private void onKeyMappings(RegisterKeyMappingsEvent event) {
+		ModKeybinds.init(event);
 	}
 	
 }
